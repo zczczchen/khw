3944eb0d4f647101dc7dc6835800940fab3a19bd
ZC <>
2024-02-14 16:47:46 +0800

    header position & logo rwd

--- react-practice/src/App.jsx
+++ react-practice/src/App.jsx
@@ -1,15 +1,22 @@
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { useMediaQuery } from "react-responsive";
 import Home from "./pages/Home";
 import Counter from "./pages/Counter";
 import "./css/layout.css";
 
 function App() {
+  const isTabletOrMobile = useMediaQuery({ query: "(max-width: 425px)" });
+
   return (
     <BrowserRouter>
       <header className='header'>
         <nav className='nav'>
           <Link className='logo-link' to='/'>
-            <img className='logo-img' src='/logo.svg' alt='Logo' />
+            {isTabletOrMobile ? (
+              <img className='logo-img' src='/logo_s.svg' alt='Logo' />
+            ) : (
+              <img className='logo-img' src='/logo.svg' alt='Logo' />
+            )}
           </Link>
         </nav>
       </header>
