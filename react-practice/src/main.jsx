3220f5a66b1486a2c7946dae437e6a5c34dbe9e6
ZC <>
2024-11-08 17:07:42 +0800

    Revert "change BrowserRouter to HashRouter"
    This reverts commit 3b07494df62339343b9d362a1522dac4cfdf4134.

--- react-practice/src/main.jsx
+++ react-practice/src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { HashRouter as Router } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Router basename='/khw'>
+    <BrowserRouter basename='/khw'>
       <App />
-    </Router>
+    </BrowserRouter>
   </React.StrictMode>
 );
