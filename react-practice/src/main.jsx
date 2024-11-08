3b07494df62339343b9d362a1522dac4cfdf4134
ZC <>
2024-11-08 16:07:53 +0800

    change BrowserRouter to HashRouter

--- react-practice/src/main.jsx
+++ react-practice/src/main.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter basename='/khw'>
+    <Router basename='/khw'>
       <App />
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
